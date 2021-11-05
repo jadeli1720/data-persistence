0313e867a01d554a81bb18edcf0c08f597b2a000
jadeli1720 <jadeli1720@gmail.com>
2021-11-04 19:04:06 -0600

    Server up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: 'up'})
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // start your server here
+require('dotenv').config();
+
+const server = require('./api/server');
+const defaults = require('./config/default');
+
+const port = defaults.port;
+
+server.listen(port, () => console.log(`\n** Server up on port ${port} **\n`))
